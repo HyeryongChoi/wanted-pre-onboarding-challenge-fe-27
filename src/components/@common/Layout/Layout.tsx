a1c211dd60e46c202e77f69a52ec5facc88a68cc
HyeryongChoi <chex1004@gmail.com>
2024-11-07 11:46:43 +0900

    chore: install and apply mui

--- src/components/@common/Layout/Layout.tsx
+++ src/components/@common/Layout/Layout.tsx
@@ -1,7 +1,12 @@
+import { Container } from '@mui/material';
 import { PropsWithChildren } from 'react';
 
 function Layout({ children }: PropsWithChildren) {
-  return <main>{children}</main>;
+  return (
+    <Container maxWidth='xl' component='main' sx={{ backgroundColor: 'aliceblue', height: '100vh', padding: '20px' }}>
+      {children}
+    </Container>
+  );
 }
 
 export default Layout;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import { CssBaseline } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
@@ -11,6 +11,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <CssBaseline />
       <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
