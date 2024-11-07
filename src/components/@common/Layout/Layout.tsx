e4871e240d1fa1d54e9edf18f30288d7b72927d3
HyeryongChoi <chex1004@gmail.com>
2024-11-07 23:01:46 +0900

    feat: change Layout background color

--- src/components/@common/Layout/Layout.tsx
+++ src/components/@common/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react';
 
 function Layout({ children }: PropsWithChildren) {
   return (
-    <Container maxWidth='xl' component='main' sx={{ backgroundColor: 'aliceblue', height: '100vh', padding: '20px' }}>
+    <Container maxWidth='xl' component='main' sx={{ height: '100vh', padding: 2 }}>
       {children}
     </Container>
   );
