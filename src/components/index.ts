ddab321a60a08b1fec384db3b9e75d6991ec96e7
HyeryongChoi <chex1004@gmail.com>
2024-11-07 23:21:01 +0900

    feat: add toggle feature to expand/collapse todo item details

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
+export { default as TodoDetail } from './TodoDetail/TodoDetail';
 export { default as TodoItem } from './TodoItem/TodoItem';
+export { default as TodoList } from './TodoList/TodoList';
